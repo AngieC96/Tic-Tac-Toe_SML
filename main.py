80788e52adbe2c22f60a7fb91081ca30326107e5
Andrea Gasparin <andrea.gasparin.87@gmail.com>
2020-06-24 22:59:40 +0200

    pareggiante

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from TicGame.tic_game import TicGame
 from Training.trainer import AITrainer
 from Training.training import TicTraining
 from Players.randomPlayer import RandomPlayer
+from Players.randomMemory import RandomMemory
 
 SAMPLE_SIZE = 200 #1024 * 5
 CAPACITY = 1_000_000
@@ -15,26 +16,29 @@ REWARD_LOSE = -1
 FIXED_BATCH = False
 only_valid_moves = True
 EPS_MIN: float = 0.1
-NUM_GAMES = 1500 #50_000
+NUM_GAMES = 15_000 #50_000
 EPS_DECAY: float = 1000
 UPDATE_TARGET_EVERY = 20
-STUPID_PLAYER_RANDOMNESS = 1
+STUPID_PLAYER_RANDOMNESS = 0
 
 boardsize = 3
 
 
 trainer = AITrainer(boardsize, REWARD_INVALID_SCORE, REWARD_WIN, REWARD_LOSE, SAMPLE_SIZE, CAPACITY, GAMMA,
                     EPS_MIN, EPS_DECAY, fixed_batch=FIXED_BATCH, double_q_interval=UPDATE_TARGET_EVERY)
-
+randomMemoryPlayer =RandomMemory(boardsize,REWARD_INVALID_SCORE, REWARD_WIN, REWARD_LOSE, trainer.replayMemory)
 board_size = 3
-players = [RandomPlayer(board_size), trainer]
+players = [RandomPlayer(board_size, STUPID_PLAYER_RANDOMNESS), trainer]
 game = TicTraining(players, board_size)
+game_memory = TicTraining([RandomPlayer(board_size, STUPID_PLAYER_RANDOMNESS),randomMemoryPlayer],boardsize )
 count_invalid = 0
 count_win = 0
 count_draws = 0
 count_lose = 0
 
 for i in range(NUM_GAMES):
+    game_memory.play()
+    game_memory.reset()
     if game.play():             #no invalid moves
         if game.winner == True:
             if trainer.winner == True: count_win += 1
